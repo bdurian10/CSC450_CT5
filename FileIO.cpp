13cc418ff47a54b808baa8ed68a4a0c8eda77832
Braden Durian <braden.durian@csuglobal.edu>
2024-07-11 20:17:37 -0600

    Second Commit: implemented file reversal method

--- FileIO.cpp
+++ FileIO.cpp
@@ -6,22 +6,73 @@
 */
 #include <iostream>
 #include <fstream>
+#include <stack>
+#include <string>
+#include <sstream>
 
 using namespace std;
+
+string reverse_text(const string& filename)
+{
+	string content;
+	stringstream buffer;
+	stack<char> stack;
+
+	ifstream file(filename);
+
+	if(!file.is_open())
+	{
+		cerr << "Unable to open file" << endl;
+		return "";
+	}
+
+	//Read entire file into a stringstream
+	buffer << file.rdbuf();
+
+	//Convert stringstream into a string
+	content = buffer.str();
+
+	//Read each character in content and add to stack;
+	for(char c : content)
+	{
+		stack.push(c);
+	}
+
+	//Clear content string
+	content.clear();
+
+	//Add characters from stack in reverse order
+	while(!stack.empty())
+	{
+		content.push_back(stack.top());
+		stack.pop();
+	}
+
+	return content;
+}
 
 int main()
 {
 
 	fstream fio;
 	string line;
+	string inputFile = "CSC450_CT5_mod5.txt";
+	string reversedFile = "CSC450_CT5_mod5_reverse.txt";
 
-	fio.open("CSC450_CT5_mod5.txt", ios::app | ios::out | ios::in);
+	fio.open(inputFile, ios::in | ios::out | ios::app);
 
-	cout << "Enter your text. Enter '-1' to exit..." << endl;
+	cout << "Enter your text. Enter '-1' to when complete..." << endl;
 
-	while(fio)
+	if(!fio.is_open())
 	{
-		//read line from input
+		cerr << "Unable to open file" << endl;
+		return 1;
+	}
+
+	//Loop for accepting user input until '-1' is entered
+	while(true)
+	{
+		//Read line from input
 		getline(cin, line);
 
 		if(line == "-1")
@@ -29,24 +80,61 @@ int main()
 
 		//Write line to file
 		fio << line << endl;
-
 	}
 
-	//point read pointer to beginning of file
+	//Point read pointer to beginning of file
+	//Must use this between input and output, per SEI CERT C++
 	fio.seekg(0, ios::beg);
 
-	while(fio)
-	{
-		//read line from file
-		getline(fio, line);
+	cout << "Contents of: " << inputFile << endl;
 
+	//read line from file
+	while(getline(fio, line))
+	{
 		//Print line
 		cout << line << endl;
 	}
 
 	//Close file
 	fio.close();
+	cout << endl;
 
+	string reversal;
+	reversal = reverse_text(inputFile);
+
+	ofstream reverse_fio(reversedFile);
+
+	if(!reverse_fio.is_open())
+	{
+		cerr << "Unable to open file" << endl;
+		return 1;
+	}
+
+	cout << "Saving Reversed file to: " << reversedFile << endl;
+
+	reverse_fio << reversal;
+	reverse_fio.close();
+
+	//Open reversed file in input mode to display content
+	ifstream reversed_input(reversedFile);
+
+	if(!reversed_input.is_open())
+	{
+		cerr << "Unable to open file" << endl;
+		return 1;
+	}
+
+	cout << "Reversed File Content: " << endl;
+
+	//Print reverse file
+	while(getline(reversed_input, line))
+	{
+		cout << line << endl;
+	}
+	//Close reverse file
+	reversed_input.close();
 
 	return 0;
 }
+
+
